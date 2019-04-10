9ebd64c5a7d7810565a52a0eec04d9f2c693753a
Marlon Monzon <martianworm17@gmail.com>
2019-04-10 17:52:59 +0800

    Average

---
+++ src/com/example/Variables.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        int firstNumber;
+        int secondNumber;
+
+        firstNumber = 11;
+        secondNumber = 20;
+
+        double average;
+
+        average = (double) (firstNumber + secondNumber) / 2;
+
+        System.out.println("Average: " + average);
+    }
+}

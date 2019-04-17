9c41bfda32b134c273700b0e82b033e2c7a5f2ea
Marlon Monzon <martianworm17@gmail.com>
2019-04-17 22:24:03 +0800

    Comments in Java

---
+++ src/com/comments/Comments.java
@@ -0,0 +1,21 @@
+package com.comments;
+
+public class Comments {
+
+    /**
+     * This is the main method of the class
+     * @param args This is just a variable
+     */
+    public static void main(String[] args) {
+        int firstNumber = 11;
+        int secondNumber = 12; // This is the second number entered
+
+        // This is the average between the first and the second number
+        double average = (firstNumber + secondNumber) / 2.0;
+
+        /*
+            Hello World
+            How are you?!
+         */
+    }
+}

dbdd912915b4294a94bcf400e714e3187ac0fac6
Marlon Monzon <martianworm17@gmail.com>
2019-04-18 21:41:22 +0800

    Decision Structures

---
+++ src/com/decisions/DecisionStructures.java
@@ -0,0 +1,14 @@
+package com.decisions;
+
+public class DecisionStructures {
+
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 6;
+        int p = 7;
+
+        boolean expression = !(x < y);
+
+        System.out.println(expression);
+    }
+}

28600a6edbe6a6cf6aeeb200a97bff22bf6761c4
Marlon Monzon <martianworm17@gmail.com>
2019-04-18 23:41:03 +0800

    Comparing Strings

--- src/com/compare/CompareStrings.java
+++ src/com/compare/CompareStrings.java
@@ -8,7 +8,7 @@ public class CompareStrings {
         String name1 = JOptionPane.showInputDialog("Please enter name 1: ");
         String name2 = JOptionPane.showInputDialog("Please enter name 2: ");
 
-        if (name1 == name2) {
+        if (name1.equals(name2)) {
             System.out.println("name 1 is equals to name 2");
         } else {
             System.out.println("Not the same!");
